93d656d99939442483b2d1b78a15db080a7ee9a6
Quinn Slack <qslack@qslack.com>
2015-01-29 21:06:18 -0800

    better interface for fetchers

--- net.go
+++ net.go
@@ -1,15 +1,14 @@
 package rwvfs
 
-// ExplicitFetchOpener is implemented by FileSystems that support a
-// mode of operation where Open returns a lazily loaded file. This is
+import "golang.org/x/tools/godoc/vfs"
+
+// FetcherOpener is implemented by FileSystems that support a mode of
+// operation where OpenFetcher returns a lazily loaded file. This is
 // useful for VFS implementations that are backed by slow (e.g.,
 // network) data sources, so you can explicit fetch byte ranges at a
 // higher level than Go I/O buffering.
-type ExplicitFetchOpener interface {
-	// ExplicitFetch sets whether Open should return files that must
-	// be explicit fetched (i.e., that implement the Fetcher
-	// interface).
-	ExplicitFetch(bool)
+type FetcherOpener interface {
+	OpenFetcher(name string) (vfs.ReadSeekCloser, error)
 }
 
 // Fetcher is implemented by files that require explicit fetching to
